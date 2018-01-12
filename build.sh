9d9913b372a55e0340c4b3a61a3fd27c97681aa4
Auston Stewart <auston@shedbuilt.net>
2018-01-12 02:16:22 -0500

    Adding the toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-sed -i 's/usr/tools/' build-aux/help2man
-sed -i 's/testsuite.panic-tests.sh//' Makefile.in
-./configure --prefix=/usr --bindir=/bin
-make -j $SHED_NUMJOBS
-make html
-make DESTDIR=${SHED_FAKEROOT} install
-install -d -m755 ${SHED_FAKEROOT}/usr/share/doc/sed-4.4
-install -m644 doc/sed.html ${SHED_FAKEROOT}/usr/share/doc/sed-4.4
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools || return 1
+        make -j $SHED_NUMJOBS || return 1
+        make DESTDIR="$SHED_FAKEROOT" install || return 1
+        ;;
+    *)
+        sed -i 's/usr/tools/' build-aux/help2man
+        sed -i 's/testsuite.panic-tests.sh//' Makefile.in
+        ./configure --prefix=/usr --bindir=/bin || return 1
+        make -j $SHED_NUMJOBS || return 1
+        make html || return 1
+        make DESTDIR="$SHED_FAKEROOT" install || return 1
+        install -d -m755 "${SHED_FAKEROOT}/usr/share/doc/sed-4.4"
+        install -m644 doc/sed.html "${SHED_FAKEROOT}/usr/share/doc/sed-4.4"
+        ;;
+esac
