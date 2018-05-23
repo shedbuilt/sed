55430c5f73519ed5fd96b6bec1c9adbc3a203c00
Auston Stewart <auston@shedbuilt.net>
2018-05-22 17:04:07 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        ./configure --prefix=/tools &&
-        make -j $SHED_NUM_JOBS &&
-        make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-        ;;
-    *)
-        sed -i 's/usr/tools/' build-aux/help2man &&
-        sed -i 's/testsuite.panic-tests.sh//' Makefile.in &&
-        ./configure --prefix=/usr --bindir=/bin &&
-        make -j $SHED_NUM_JOBS || exit 1
-        if $SHED_INSTALL_DOCS; then
-            make html || exit 1
-        fi
-        make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-        if $SHED_INSTALL_DOCS; then
-            install -d -m755 "${SHED_FAKE_ROOT}/usr/share/doc/sed-${SHED_PKG_VERSION}" &&
-            install -m644 doc/sed.html "${SHED_FAKE_ROOT}/usr/share/doc/sed-${SHED_PKG_VERSION}"
-        fi
-        ;;
-esac
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+SHED_PKG_LOCAL_PREFIX='/usr'
+SHED_PKG_LOCAL_BINDIR='/bin'
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    SHED_PKG_LOCAL_PREFIX='/tools'
+    SHED_PKG_LOCAL_BINDIR='/tools/bin'
+elif [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ]; then
+    sed -i 's/usr/tools/' build-aux/help2man || exit 1
+fi
+sed -i 's/testsuite.panic-tests.sh//' Makefile.in &&
+./configure --prefix=${SHED_PKG_LOCAL_PREFIX} \
+            --bindir=${SHED_PKG_LOCAL_BINDIR} &&
+
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    make html &&
+    install -Dm644 doc/sed.html "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_PREFIX}/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}/sed.html"
+fi
